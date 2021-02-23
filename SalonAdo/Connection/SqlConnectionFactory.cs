8e46b75069d689e9f9987e2758441562cc4bb492
Roman Hulovatyi <roman.hulovatuy@gmail.com>
2021-02-23 13:24:37 +0200

    test

--- SalonAdo/Connection/SqlConnectionFactory.cs
+++ SalonAdo/Connection/SqlConnectionFactory.cs
@@ -10,6 +10,7 @@ namespace Salon.ADO.DAL.Connection
     public class SqlConnectionFactory : ISqlConnectionFactory
     {
         //private readonly string connection = @"Data Source = ROMAN; Initial Catalog = Salon; Integrated Security = True";
+        //coment
         private readonly string connection = @"Server=tcp:romanhulovatyi.database.windows.net,1433;Initial Catalog=Salon;Persist Security Info=False;User ID=roman;Password=Qwerasdfzxcv1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
 
         public SqlConnection CreateSqlConnection()
