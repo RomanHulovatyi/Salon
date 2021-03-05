136edb4749ccfa1381bae3beeb152d6d220de59c
Roman Hulovatyi <roman.hulovatuy@gmail.com>
2021-03-05 19:45:19 +0200

    Revert "another check"
    This reverts commit 243b75a8e616377656397ebebee1d966262247b6.

--- Salon.nUnit/BLL/CustomerManagerTest.cs
+++ Salon.nUnit/BLL/CustomerManagerTest.cs
@@ -62,7 +62,7 @@ namespace Salon.nUnit.BLL
             customerManager.Get();
 
             //Assert
-            mock.Verify(x => x.GetList(), Times.Once());
+            mock.Verify(x => x.GetList(), Times.Exactly(2));
         }
 
         [Test]
